3a21b0f92ffdd88769df61da0046c1e1f12bea21
zh <chua0763@e.ntu.edu.sg>
2019-08-26 16:01:58 +0800

    Colour integer to bit
    Saving images is currently not working due to bit shifting problems.

--- loltest/src/loltest/ImageFile.java
+++ loltest/src/loltest/ImageFile.java
@@ -3,6 +3,7 @@ package loltest;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
@@ -60,27 +61,44 @@ public class ImageFile {
 		return this.pathname;
 	}
 	
+	/**	Loads file attribute from pathname.
+	 * 
+	 */
 	public void loadFile(){
 		this.file = new File(this.pathname);
 	}
 	
+	/**	Saves file. Requires an existing image attribute.
+	 * 
+	 * @throws IOException
+	 */
 	public void saveFile() throws IOException{
-		try {
-			this.file.createNewFile();
-		} catch(Exception e){
-			System.out.println("Unable to create new file");
-		}
+	       this.file = new File(pathname);
+	       ImageIO.write(this.image, "png", this.file);
 	}
 	
+	/** Reads existing image attribute to set width and height.
+	 * 
+	 */
 	public void generateWidthHeight(){
 		setWidth(this.image.getWidth());
 		setHeight(this.image.getHeight()); 
 	}
 	
+	/**	Reads known File attribute into image attribute.
+	 * 
+	 * @throws IOException
+	 */
 	public void readFileToImage() throws IOException{
 		this.image = ImageIO.read(this.file);
 	}
 	
+	/**	Generates data for a single pix using data from BufferedImage pixel.
+	 * 
+	 * @param colour	integer colour (NOT COLOUR CLASS!) in ARGB form.
+	 * @param x
+	 * @param y
+	 */
 	public void generatePixForImageObj(int colour, int x, int y){
 		float a = (float) ((colour>>24) & 0xff);
 		float r = (float) ((colour>>16) & 0xff);
@@ -90,6 +108,9 @@ public class ImageFile {
 		imageObj.setPixIntoPixArray(new Pix( new Colour(a,r,g,b), new Coord(x,y)));
 	}
 	
+	/** Traverses image object to populate imageObj, pixelwise.
+	 * 
+	 */
 	public void generateImageObjDetailsFromImage(){
 		for(int checkX = 0; checkX < this.getWidth(); checkX++){
 			for(int checkY = 0; checkY < this.getHeight(); checkY++){
@@ -98,6 +119,10 @@ public class ImageFile {
 		}
 	}
 	
+	/** Void function to create new imageObj 
+	 * and generate the imageObj's data from image.
+	 * 
+	 */
 	public void generateImageObj(){
 		if(file == null){
 			System.out.println("File does not Exist");
@@ -107,17 +132,101 @@ public class ImageFile {
 		generateImageObjDetailsFromImage();
 	}
 	
-	// sets imagefile pixel by pixel using info from imageobj pix
+	/** Returns the integer of n to the power of p. Integers only.
+	 * 
+	 * @param n
+	 * @param p
+	 * @return
+	 */
+	public static int pow(int n, int p){
+		// cant fucking believe i need to write this
+		System.out.println("pow");
+		if(p == 0){
+			return 1;
+		}
+		if(p == 1){
+			return n;
+		}
+		int prod = n;
+		for(int i = 2; i <= p; i++){
+			prod = prod*n;
+		}
+		return prod;
+	}
+	
+	/** Get an ArrayList representing the bit pattern of an integer
+	 * 
+	 * @param num
+	 * @return
+	 */
+	public static ArrayList<Integer> getPower(int num){
+		System.out.println("getPower");
+		ArrayList<Integer> bits = new ArrayList<Integer>();
+		// 128, 64, 32, 16, 8, 4, 2, 1
+		for( int i = 7; i >= 0 ; i--){
+			if(num / pow(2,i) >= 1){
+				bits.add(1);
+				num = num - pow(2,i);
+			}
+			else bits.add(0);
+		}
+		return bits;
+	}
+	
+	/** Get an ArrayList representing the bit pattern of the Alpha Channel (8-bits)
+	 * 
+	 * @param a
+	 * @return
+	 */
+	public static ArrayList<Integer> getAinBitForm(int a){
+		System.out.println("getAinBitForm");
+		return getPower(a);
+	}
+	
+	
+	public static void main(String[] args){
+		ArrayList<Integer> a = getAinBitForm(55);
+		for(int i = 0; i<a.size(); i++){
+			System.out.print(a.get(i));
+		}
+	}
+	
+	/** Sets a pixel of imageFile using info (colour, coord) from a pix of imageObj
+	 * 
+	 * @param pix
+	 */
 	public void setImageFilePixByImageObjPix(Pix pix){
-		int rgb = ((int) pix.getColour().getA() <<24) | 
+/*		int rgb = ((int) pix.getColour().getA() <<24) | 
 				((int) pix.getColour().getR()<<16) | 
-				((int) pix.getColour().getG()<<8) | (int) pix.getColour().getB();
+				((int) pix.getColour().getG()<<8) | (int) pix.getColour().getB();*/
+		int rgb = -1;
+		System.out.println(Integer.toBinaryString(rgb));
+		
+		System.out.println(Integer.toBinaryString((int) pix.getColour().getA()));
+		rgb = ((int)pix.getColour().getA() << 24) & rgb;
+		System.out.println(Integer.toBinaryString(rgb));
+	
+		System.out.println(Integer.toBinaryString((int) pix.getColour().getR()));
+		rgb = (rgb & ~(0xFF << 16)) | ((int)pix.getColour().getR() << 16);
+		System.out.println(Integer.toBinaryString(rgb));
+		
+		System.out.println(Integer.toBinaryString((int) pix.getColour().getG()));
+		rgb = (rgb & ~(0xFF << 8)) | ((int)pix.getColour().getG() << 8);
+		System.out.println(Integer.toBinaryString(rgb));
+		
+		System.out.println(Integer.toBinaryString((int) pix.getColour().getB()));
+		rgb = (rgb & ~(0xFF)) | ((int)pix.getColour().getB());
+		System.out.println(Integer.toBinaryString(rgb));
+		
 		this.image.setRGB(pix.getCoord().getX(), pix.getCoord().getY(), rgb);
 	}
 	
-	// populates a new imagefile with data from imageobj
+	/** Populates a new imageFile's pixels with pixel data from imageObj
+	 * 
+	 * @param imageObj
+	 */
 	public void generateImageFromImageObj(ImageObj imageObj){
-		this.image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		this.image = new BufferedImage(imageObj.getWidth(), imageObj.getHeight(), BufferedImage.TYPE_INT_ARGB);
 		for(int pixInx = 0; pixInx < imageObj.getPixArray().size(); pixInx++){
 			setImageFilePixByImageObjPix(imageObj.getPixArray().get(pixInx));
 		}
--- loltest/src/loltest/Testmain.java
+++ loltest/src/loltest/Testmain.java
@@ -10,33 +10,55 @@ public class Testmain {
 	}
 
 	public static void printImageFileInfo() throws IOException{
-		ImageFile imgF = new ImageFile("D:\\image\\test88white.png");
+		ImageFile imgF = new ImageFile("D:\\image\\test1x1black.png");
 		imgF.loadFile();
 		imgF.readFileToImage();
 		imgF.generateWidthHeight();
 		imgF.generateImageObj();
-		for(int i = 0; i < imgF.getImageObj().getPixArray().size(); i++){
-			
+		ImageFile imgF2 = new ImageFile("D:\\image\\savetest.png");
+		imgF2.setImageObj(imgF.getImageObj());
+		imgF2.generateImageFromImageObj(imgF2.getImageObj());
+		imgF2.saveFile();
 		
-			System.out.print(imgF.getImageObj().getPixArray().get(i).getCoord().
-					getX());
-			System.out.print("    ");
-			System.out.print(imgF.getImageObj().getPixArray().get(i).getCoord().
-					getY());
-			System.out.print("        ");
-			
-			System.out.print(imgF.getImageObj().getPixArray().get(i).getColour().
-					getA());
-			System.out.print("    ");
-			System.out.print(imgF.getImageObj().getPixArray().get(i).getColour().
-					getR());
-			System.out.print("    ");
-			System.out.print(imgF.getImageObj().getPixArray().get(i).getColour().
-					getG());
-			System.out.print("    ");
-			System.out.print(imgF.getImageObj().getPixArray().get(i).getColour().
-					getB());
-			System.out.print("\n");
+		// SAVING IS CURRENTLY BUGGED
+		// COLOUR ASSIGNMENT IS INCORRECT
+		for(int i = 0; i < imgF.getImageObj().getPixArray().size(); i++){
+			System.out.println(Integer.toBinaryString(imgF.getImage().getRGB(0, 0)));
+			System.out.println(Integer.toBinaryString(imgF2.getImage().getRGB(0, 0)));
+//			
+//		
+//			System.out.print(imgF.getImageObj().getPixArray().get(i).getCoord().
+//					getX());
+//			System.out.print("    ");
+//			System.out.print(imgF.getImageObj().getPixArray().get(i).getCoord().
+//					getY());
+//			System.out.print("        ");
+//			
+//			System.out.print(imgF.getImageObj().getPixArray().get(i).getColour().
+//					getA());
+//			System.out.print("    ");
+//			System.out.print(imgF.getImageObj().getPixArray().get(i).getColour().
+//					getR());
+//			System.out.print("    ");
+//			System.out.print(imgF.getImageObj().getPixArray().get(i).getColour().
+//					getG());
+//			System.out.print("    ");
+//			System.out.print(imgF.getImageObj().getPixArray().get(i).getColour().
+//					getB());
+//			System.out.print("        ");
+//			
+//			System.out.print(imgF2.getImageObj().getPixArray().get(i).getColour().
+//					getA());
+//			System.out.print("    ");
+//			System.out.print(imgF2.getImageObj().getPixArray().get(i).getColour().
+//					getR());
+//			System.out.print("    ");
+//			System.out.print(imgF2.getImageObj().getPixArray().get(i).getColour().
+//					getG());
+//			System.out.print("    ");
+//			System.out.print(imgF2.getImageObj().getPixArray().get(i).getColour().
+//					getB());
+//			System.out.print("\n");
 		}
 	}
 }
