fe3067ca11022b7fbb016bf5b15d1804d55ce7ee
zh <chua0763@e.ntu.edu.sg>
2019-08-29 18:06:36 +0800

    Angle/Direction finding done
    Untested, 
    Distance finding still incomplete

--- loltest/src/loltest/DisruptionTemplate.java
+++ loltest/src/loltest/DisruptionTemplate.java
@@ -45,6 +45,43 @@ public class DisruptionTemplate extends ImageObj {
 		return null;
 	}
 	*/
+	
+	/** Determines equality between two floats to 0.001% precision of either value
+	 * 
+	 * @param fl1
+	 * @param fl2
+	 * @return
+	 */
+	public boolean flEq(float fl1, float fl2){
+		float errMar1 = (float) ((0.001) * (fl1/100));
+		float errMar2 = (float) ((0.001) * (fl2/100));
+		
+		if(Math.abs(fl1 - fl2) < errMar1 && Math.abs(fl1-fl2) < errMar2){
+			return true;
+		}
+		return false;
+	}
+	
+	/** Determines equality between two floats to a given precision of either value
+	 * 
+	 * @param fl1
+	 * @param fl2
+	 * @return
+	 */
+	public boolean flEq(float fl1, float fl2, float pres){
+		float errMar1 = (float) ((pres) * (fl1/100));
+		float errMar2 = (float) ((pres) * (fl2/100));
+		
+		if(Math.abs(fl1 - fl2) < errMar1 && Math.abs(fl1-fl2) < errMar2){
+			return true;
+		}
+		return false;
+	}
+	
+	/** Unused function in code to space getters&setters from actual functions
+	 * 
+	 */
+	private void SPACER(){}
 
 	/** Returns the angle that should be checked currently.
 	 * 
@@ -58,15 +95,119 @@ public class DisruptionTemplate extends ImageObj {
 		return (float)count * (((float)360)/((float)angleInterval));
 	}
 	
-	/** 
+	/** Calculates the appropriate change in Y per unit X traversed
+	 * i.e. find vertical component of angle
+	 * X may traverse 1 or -1 in actuality but this doesn't matter here.
+	 * For straight up/down cases, X traverses zero but Y can be any value.
+	 * 
+	 * @param angle
+	 * @return
+	 */
+	public float getYXRatio(float angle){
+		// straight up, 0 deg
+		if(angleIsUp(angle) && !angleIsLeft(angle) && !angleIsRight(angle)){
+			return 1;
+		}
+		// straight down, 180 deg
+		if(angleIsDown(angle) && !angleIsLeft(angle) && !angleIsRight(angle)){
+			return -1;
+		}
+		// 0 - 90 deg quadrant
+		if(angleIsUp(angle) && angleIsRight(angle)){
+			return (float) Math.tan(Math.toRadians(90 - angle));
+		}
+		// 90 - 180 deg quadrant
+		if(angleIsDown(angle) && angleIsRight(angle)){
+			return 0 - (float) Math.tan(Math.toRadians(angle - 90));
+		}
+		// 180 - 270 deg quadrant
+		if(angleIsDown(angle) && angleIsLeft(angle)){
+			return 0 - (float) Math.tan(Math.toRadians(270 - angle));
+		}
+		// 270 - 260 deg quadrant
+		if(angleIsUp(angle) && angleIsLeft(angle)){
+			return (float) Math.tan(Math.toRadians(angle - 270));
+		}
+		// straight left or straight right
+		if(!angleIsUp(angle) && !angleIsDown(angle)){
+			return 0;
+		}
+		return 0;
+	}
+	
+	/** Checks if angle has leftward horizontal component.
+	 * 
+	 * @param angle
+	 * @return
+	 */
+	public boolean angleIsLeft(float angle){
+		if(angle > 180 && angle < 360){
+			return true;
+		}
+		return false;
+	}
+	
+	/** Checks if angle has rightward horizontal component.
+	 * 
+	 * @param angle
+	 * @return
+	 */
+	public boolean angleIsRight(float angle){
+		if(angle > 180 && angle < 360){
+			return true;
+		}
+		return false;
+	}
+	
+	/** Checks if angle has upward vertical component.
+	 * 
+	 * @param angle
+	 * @return
+	 */
+	public boolean angleIsUp(float angle){
+		if(angle > 0 && angle < 90){
+			return true;
+		}
+		if(angle > 270 && angle < 360){
+			return true;
+		}
+		return false;
+	}
+	
+	/** Checks if angle has downward vertical component.
+	 * 
+	 * @param angle
+	 * @return
+	 */
+	public boolean angleIsDown(float angle){
+		if(angle > 90 && angle < 270){
+			return true;
+		}
+		return false;
+	}
+	
+	/** Finds distance to pix on other side of given shape, from the given pix,
+	 * at the given angle.
+	 * 
+	 * Straight-up is considered 0deg, goes clockwise.
+	 * 
+	 * If no pixs are found with the angle, return 0
 	 * 
 	 * @param shape
 	 * @param pix
 	 * @param angle
 	 * @return
 	 */
-	public int getDistanceToOtherEdge(Shape shape, Pix pix, float angle){
-		
+	// neon for extranet
+	// latest for eserv
+	public float getDistanceToOtherEdge(Shape shape, Pix pix, float angle){
+		for(int pixInx = 0; pixInx < shape.getPixArray().size(); pixInx++){
+			if( shape.getPixArray().get(pixInx) != pix 
+					&& shape.pixIsShapeEdge(shape.getPixArray().get(pixInx))){
+				
+			}
+		}
+		return 1;
 	}
 	
 	/**  Generates single disruptionStat by drawing line from origin (pix)
